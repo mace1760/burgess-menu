b6cebab789b6efddb5d48e05655148e9747389e7
mace1760 <macenumummaw2003@gmail.com>
2025-05-31 15:26:59 -0400

    Almost semi-complete

--- javascript/menu-change.js
+++ javascript/menu-change.js
@@ -1,26 +1,54 @@
 // Page made to switch depending on what is choosen for the menu that week
 
-
-
 /*
+    Types:
+    mediterranean
 
-key
+*/
 
-mediterranean = 1
 
-*/ 
+// type of food for that day
+let type = "mediterranean";
 
-key = 1;
+// each type can have different menus, what menu of that type (menu1, menu2, menu3...)
+let number = 1;
 
 
 
 // ---------- mediterranean ----------
 
-// breakfast
-if (hour = 1) {
-    $(document).ready(function () {
-        $("#breakfast-link").attr("href", "menus/mediterranean/mediterranean-breakfast1.html");
-        $("#lunch-link").attr("href", "menus/mediterranean/mediterranean-lunch1.html");
-        $("#dinner-link").attr("href", "menus/mediterranean/mediterranean-lunch1.html");
-    });
+
+function concatnate(time, type, number) {
+
+    if (time == "breakfast"){
+        let breakfastsetup = "menus/" + type + "/" + type + "-breakfast" + number + ".html";
+        return breakfastsetup;
+    }
+
+    if (time == "lunch"){
+        let lunchsetup = "menus/" + type +  "/" + type +"-lunch" + number + ".html";
+        return lunchsetup;
+    }
+
+    if (time == "dinner"){
+        let dinnersetup = "menus/" + type +  "/" + type +"-dinner" + number + ".html";
+        return dinnersetup;
+    }
+    
 }
+
+// -------------------- Day selection --------------------
+
+let breakfast = concatnate("breakfast", type, number);
+let lunch = concatnate("lunch", type, number);
+let dinner = concatnate("dinner", type, number)
+
+$(document).ready(function () {
+    $("#breakfast-link").attr("href", breakfast);
+    $("#lunch-link").attr("href", lunch);
+    $("#dinner-link").attr("href", dinner);
+});
+
+
+
+
