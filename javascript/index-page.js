6d5e75508618f08d8a83a4c548722260ae1df528
mace1760 <macenumummaw2003@gmail.com>
2025-06-01 15:40:22 -0400

    Javascript applied to index-page, Italian pages started

---
+++ javascript/index-page.js
@@ -0,0 +1,5 @@
+// This Page will deliever the content that will show on the index-page
+
+const selectElement = document.getElementById('food-type');
+const selectedValue = selectElement.value;
+console.log(selectedValue);
